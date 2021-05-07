dc3a2a5d2f3af5a472db00b60cabf6ae783d7947
Patrick Easters <peasters@redhat.com>
2021-05-07 13:49:24 -0400

    Add clowder config adapter

--- cmd/caddy/caddy.go
+++ cmd/caddy/caddy.go
@@ -4,6 +4,7 @@ import (
 	caddycmd "github.com/caddyserver/caddy/v2/cmd"
 
 	// Caddy modules
+	_ "github.com/RedHatInsights/rhsm-api-proxy/modules/clowder"
 	_ "github.com/RedHatInsights/rhsm-api-proxy/modules/rbac"
 	_ "github.com/caddyserver/caddy/v2/modules/standard"
 	_ "github.com/iamd3vil/caddy_yaml_adapter"
